cc854768ea4032fadc8e4fbe3f86ee35cde448d1
Manimala Badugu <manibadugu5@gmail.com>
2023-02-23 23:50:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const Posts=require('./models/postmodel')
+const express=require('express')
+const app=express()
+
+app.use(express.json())
+
+app.post('/api/v1/posts', async (req,res)=>{
+    try{
+        const newPosts=await Posts.create(req.body)
+        res.status(201).json({
+            status:"Success",
+            newPosts
+        })
+    }
+    catch(err){
+        res.status(500).json({message:err.message})
+    }
+})
+
+app.get('/api/v1/allpost',async (req,res)=>{
+    try{
+        const allposts=await Posts.find()
+        res.status(200).json({
+            status:"Success",
+            allposts
+        })
+    }
+    catch(err){
+        res.status(500).json({message:err.message})
+    }
+})
+
+
+module.exports=app
